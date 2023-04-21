0a23a0dd41d4b90475300169ad99d68c2448332d
Alan Correia <csantos.alan@gmail.com>
2023-04-21 14:01:15 -0300

    Atualização Classe Filme

--- src/App.java
+++ src/App.java
@@ -1,5 +1,15 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "O Senhor dos Aneis - A sociedade do Anel";
+        meuFilme.anoDeLancamento = 1970;
+        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.avalia(9.7);
+        meuFilme.avalia(8.7);
+        
+        
+        meuFilme.exibeFichaTecnica();
+
+
     }
 }
