54343b87939a68634367896b60ca1c6a6adb527b
Alan Correia <csantos.alan@gmail.com>
2023-04-21 16:44:00 -0300

    Atualização Classe Filmes

--- src/App.java
+++ src/App.java
@@ -1,12 +1,14 @@
+import cursojava.screenmath.modelos.Filme;
+
 public class App {
     public static void main(String[] args) throws Exception {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "O Senhor dos Aneis - A sociedade do Anel";
-        meuFilme.anoDeLancamento = 1970;
-        meuFilme.duracaoEmMinutos = 180;
-        meuFilme.avalia(9.7);
+        meuFilme.setNome("O Senhor dos Aneis - A sociedade do Anel");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+        meuFilme.avalia(9.9);
         meuFilme.avalia(8.7);
-        
+
         
         meuFilme.exibeFichaTecnica();
 
