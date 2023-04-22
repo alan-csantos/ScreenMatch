5d04bcb19dbfb4f52eccd8fbdb8402215dd76e32
Alan Correia <csantos.alan@gmail.com>
2023-04-22 18:50:30 -0300

    Atualização Classe Filme

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 import cursojava.screenmath.calculos.CalculadoraDeTempo;
 import cursojava.screenmath.calculos.FiltroRecomendacao;
 import cursojava.screenmath.modelos.Episodio;
@@ -8,10 +10,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         Filme meuFilme = new Filme();
         Filme outroFilme = new Filme();
+        Filme maisUmFilme = new Filme();
         Serie minhaSerie = new Serie();
         FiltroRecomendacao filtro = new FiltroRecomendacao();
         CalculadoraDeTempo minhaCalculadora = new CalculadoraDeTempo();
         Episodio episodio = new Episodio();
+        ArrayList<Filme> listdaDeFilmes = new ArrayList<>();
 
         meuFilme.setNome("O Senhor dos Aneis - A sociedade do Anel");
         meuFilme.setAnoDeLancamento(1970);
@@ -22,6 +26,7 @@ public class App {
         minhaCalculadora.inclui(meuFilme);
         System.out.println("Tempo Calculado Atual: " + minhaCalculadora.getTempoTotal());
         filtro.filtra(meuFilme);
+        listdaDeFilmes.add(meuFilme);
 
         outroFilme.setNome("Matrix");
         outroFilme.setAnoDeLancamento(1998);
@@ -31,6 +36,23 @@ public class App {
         outroFilme.exibeFichaTecnica();
         minhaCalculadora.inclui(outroFilme);
         System.out.println("Tempo Calculado Atual: " + minhaCalculadora.getTempoTotal());
+        listdaDeFilmes.add(outroFilme);
+
+
+        maisUmFilme.setNome("Naufrago");
+        maisUmFilme.setAnoDeLancamento(1998);
+        maisUmFilme.setDuracaoEmMinutos(150);
+        maisUmFilme.avalia(9.9);
+        maisUmFilme.avalia(8.7);        
+        maisUmFilme.exibeFichaTecnica();
+        minhaCalculadora.inclui(maisUmFilme);
+        System.out.println("Tempo Calculado Atual: " + minhaCalculadora.getTempoTotal());
+        listdaDeFilmes.add(maisUmFilme);
+
+        System.out.println("Tamanho da Lista: " + listdaDeFilmes.size());
+        System.out.println("Primeiro Filme: " + listdaDeFilmes.get(0).getNome());
+        System.out.println(listdaDeFilmes                                                                                                                                                                                        );
+        System.out.println("toString do Filme: " + listdaDeFilmes.get(0).toString());
         
 
         minhaSerie.setNome("A casa do Dragão");
@@ -51,6 +73,7 @@ public class App {
         System.out.println("Serie com " + episodio.getClassificacao() + " Estrelas");
         filtro.filtra(episodio);
 
+        
 
 
 
